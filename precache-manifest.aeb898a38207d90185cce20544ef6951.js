95ca4409d98e9a0f80f824a408e571ab02db4813
葉語芯 <cynthia@hagoshintekiMBP.fios-router.home>
2020-05-28 23:59:28 -0400

    Updates

---
+++ precache-manifest.aeb898a38207d90185cce20544ef6951.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9476b0997e1bba3cce025b79a224028c",
+    "url": "/hello-data-world/index.html"
+  },
+  {
+    "revision": "79f15cdda29093044182",
+    "url": "/hello-data-world/static/css/main.04301a60.chunk.css"
+  },
+  {
+    "revision": "2fa092386aed14f2f1f4",
+    "url": "/hello-data-world/static/js/2.44fffc40.chunk.js"
+  },
+  {
+    "revision": "71772b085033e92f89cdfb3071333458",
+    "url": "/hello-data-world/static/js/2.44fffc40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79f15cdda29093044182",
+    "url": "/hello-data-world/static/js/main.2437cc54.chunk.js"
+  },
+  {
+    "revision": "b3ab0d950f52cc19b5cc",
+    "url": "/hello-data-world/static/js/runtime-main.a763d4cd.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/hello-data-world/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/hello-data-world/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "e47c3e26372c670c4a4eeac6eea0bf98",
+    "url": "/hello-data-world/static/media/airbnbLogo.e47c3e26.jpg"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/hello-data-world/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/hello-data-world/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/hello-data-world/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/hello-data-world/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/hello-data-world/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/hello-data-world/static/media/talksCardBack.0288c5ec.svg"
+  }
+]);
